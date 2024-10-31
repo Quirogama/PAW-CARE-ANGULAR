8c608554d4029919e16cd22d1c2da910043222e0
florezandres <andres.f1850@gmail.com>
2024-10-30 23:38:43 -0500

    Tratamiento v1.3
    Intento de conexion con SpringBoot, aun no funcional

--- pawcare/src/app/service/tratamiento.service.ts
+++ pawcare/src/app/service/tratamiento.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { tratamiento } from '../model/tratamiento/tratamiento';
 import { Injectable } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { tratamientoDTO } from '../model/tratamiento/tratamiento-form/tratamientoDTO';
 
 @Injectable({
   providedIn: 'root'
@@ -13,5 +15,14 @@ export class TratamientoService {
 
   addTratamiento(tratamiento: tratamiento) {
     return this.http.post('http://localhost:8080/tratamiento/add',tratamiento);
+  }
+
+  ///agregar/{cedula}/{nombredroga}/{id}
+  agregarTratamiento(cedula: number, nombredroga: string, idmascota: number, tratamientoDTO: tratamientoDTO) {
+    return this.http.post('http://localhost:8080/tratamiento/agregar/'+cedula+'/'+nombredroga+'/'+idmascota, tratamientoDTO);
+  }
+
+  findAll(): Observable<tratamiento[]> {
+    return this.http.get<tratamiento[]>('http://localhost:8080/tratamiento/all');
   }
 }
